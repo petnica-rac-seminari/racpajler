59f3db968982b8fac25cebb0690d9ce49bd138dd
blin04 <ivanbokor17@gmail.com>
2023-05-12 21:29:27 +0200

    implemented functions that loads file

--- interpreter/main.cpp
+++ interpreter/main.cpp
@@ -1,7 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<string> load() 
+{
+    ifstream in("bin/input.ir");
+
+    vector<string> tokens;
+    
+    string line = "";
+    while(getline(in, line)) {
+        // parsiranje linije
+        string token = "";
+        for (int i = 0; i < (int)line.size(); i++) {
+            if (line[i] == ' ') {
+                tokens.push_back(token);
+                token = "";
+            }
+            else token += line[i];
+        }
+        if (token != "") tokens.push_back(token);
+    }
+
+    in.close();
+
+    return tokens;
+}
+
 int main()
 {
+    vector<string> tokens = load();
+
+    for (auto s : tokens) 
+        cout << s << "\n";
+
     return 0;
 }
